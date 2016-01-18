c9a585804739bc8d011785998b75f2cb860aa10b
bhatiaankur8 <bhatia.ankur8@gmail.com>
2016-01-18 09:38:51 +0100

    Added the facebook Login feature in the mainActivity. The app id is stored in the file facebook.xml

--- app/src/main/java/com/example/ankur/pollutionandweathermonitor/MainActivity.java
+++ app/src/main/java/com/example/ankur/pollutionandweathermonitor/MainActivity.java
@@ -1,23 +1,76 @@
 package com.example.ankur.pollutionandweathermonitor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private LoginButton loginButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        if (loginButton == null) {
+            Log.v("CheckLogin", "null");
+        }
+        else {
+            Log.v("CheckLogin", "not null");
+        }
+
+
+        loginButton.setReadPermissions("user_friends");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(getApplicationContext(), "Fb Login Success", Toast.LENGTH_LONG);
+                Log.v("CheckLogin", "successfully connected to facebook");
+                //sendMessage();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Toast.makeText(getApplicationContext(), "Fb on cancel", Toast.LENGTH_LONG);
+                Log.v("CheckLogin", " connection to facebook cancelled");
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Toast.makeText(getApplicationContext(), "Fb Login Error", Toast.LENGTH_LONG);
+                Log.v("CheckLogin", "Error on  connection to facebook");
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
     }
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
