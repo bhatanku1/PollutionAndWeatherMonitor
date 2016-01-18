76fb1577133ca33833fc1397651449f37752c7f7
bhatiaankur8 <bhatia.ankur8@gmail.com>
2016-01-18 08:34:42 +0100

    Removed the effect of the custom toolbar from the mainActivity. Added the introduction text for the mainActivity

--- app/src/main/java/com/example/ankur/pollutionandweathermonitor/MainActivity.java
+++ app/src/main/java/com/example/ankur/pollutionandweathermonitor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ankur.pollutionandweathermonitor;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
     }
